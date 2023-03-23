120678123cb4807f550e42ae14b86567ce3c89b7
hassan510-cmd <hassanmahmoud607@gmail.com>
2023-03-23 23:03:25 +0200

    FEAT: translate account account name

---
+++ translate_account_account/__manifest__.py
@@ -0,0 +1,9 @@
+# write manfiest file for current module
+{
+    'name': 'Account Account',
+    'version': '0.1',
+    'depends': ['account', 'account_accountant'],
+    'installable': True,
+    'auto_install': False,
+    'author': 'Code Flex',
+}
