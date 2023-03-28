7670550fa15c70c15226ecc0d5795af35e16883c
hassan510-cmd <hassanmahmoud607@gmail.com>
2023-03-28 23:54:23 +0200

    FEAT: auto fill salary inputs

---
+++ hr_payroll_customization/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Hr Payroll Customization",
+    'summary': """  Hr Payroll Customization """,
+    'author': "Code Flex",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'depends': [
+        'hr_payroll',
+        'hr',
+    ]
+}
