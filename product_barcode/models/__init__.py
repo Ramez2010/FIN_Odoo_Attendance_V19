52143a95d8a8f0465bdcbc6afaab25d65b6ad17f
omaradel619 <omaradelfarag619@gmail.com>
2023-12-26 11:38:19 +0200

    update

--- product_barcode/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import main_category
-from . import sub_category
-from . import serial_category
-from . import vendor_category
-from . import product_template
-from . import product_product
-from . import create_receipt
