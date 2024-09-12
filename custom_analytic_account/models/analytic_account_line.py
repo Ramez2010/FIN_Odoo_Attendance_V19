a3a830b15d13a4e829ea013028908fad5f45f49e
Ezzsaleh <esaleh@onesolutionc.com>
2024-09-12 12:54:35 +0300

    constrains apt analytic

--- custom_analytic_account/models/analytic_account_line.py
+++ custom_analytic_account/models/analytic_account_line.py
@@ -10,3 +10,8 @@ class AnalyticAccountLineCustom(models.Model):
     amount = fields.Monetary(tracking=True)
     date = fields.Date(tracking=True)
     unit_amount = fields.Float(tracking=True)
+
+    @api.constrains('move_line_id')
+    def _compute_general_account_id(self):
+        for line in self:
+            line.general_account_id = line.move_line_id.account_id
