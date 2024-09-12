87835ce358475f758a9e2638de8294d7dee36a98
Ezzsaleh <esaleh@onesolutionc.com>
2024-09-12 12:56:45 +0300

    constrains apt analytic

--- custom_analytic_account/models/account_move_line.py
+++ custom_analytic_account/models/account_move_line.py
@@ -9,4 +9,16 @@ class AccountMoveLine(models.Model):
         related='move_id.analytic_account_id',
         required=False,
     )
+
     #
+
+    def _create_analytic_lines(self):
+        """ Create analytic items upon validation of an account.move.line having an analytic distribution.
+        """
+        self._validate_analytic_distribution()
+        analytic_line_vals = []
+        for line in self:
+            analytic_line_vals.extend(line._prepare_analytic_lines())
+
+        analytic_lines = self.env['account.analytic.line'].create(analytic_line_vals)
+        analytic_lines._compute_general_account_id()
