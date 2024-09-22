c3ddcc0513beaf60db18cb7cefd6f8eebc4d8b71
Ezzsaleh <esaleh@onesolutionc.com>
2024-09-22 14:46:24 +0300

    create product group

---
+++ iet_custom_access/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "iet_custom_access",
+    'version': '0.1',
+    'depends': ['base', 'sale', 'stock','product'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
