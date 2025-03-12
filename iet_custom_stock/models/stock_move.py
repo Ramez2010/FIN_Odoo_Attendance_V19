4787995cef482a57cf9273744cafd91a66e3c5fc
mohammed7874 <mohammedabdo7874@gmail.com>
2025-03-12 06:49:03 +0200

    Update Stock Picking View

--- iet_custom_stock/models/stock_move.py
+++ iet_custom_stock/models/stock_move.py
@@ -5,3 +5,9 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     arabic_name = fields.Char(related='product_id.arabic_name')
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    arabic_name = fields.Char(related='product_id.arabic_name')
