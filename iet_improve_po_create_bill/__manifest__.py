abb898c5ecd9994d48bc6c78bf0c20369b471279
noura <noura.mansour@intelligent-experts.com>
2026-01-18 15:29:29 +0200

    [Fix] description in quotation print

--- iet_improve_po_create_bill/__manifest__.py
+++ iet_improve_po_create_bill/__manifest__.py
@@ -5,13 +5,18 @@
     'category': 'Uncategorized',
     'version': '1.0',
     'license': 'LGPL-3',
-    'depends': ['base', 'purchase','sale','project','stock','sale_project'],
+    'depends': ['base', 'purchase', 'sale', 'project', 'stock', 'sale_project','account'],
     'data': [
         'views/purchase_order.xml',
         'views/sale_order.xml',
         'views/project.xml',
         'views/picking_return.xml',
     ],
+    "assets": {
+        "web.assets_backend": [
+            "iet_improve_po_create_bill/static/src/js/product_label.js",
+        ],
+    },
     'installable': True,
     'application': False,
 }
--- iet_improve_po_create_bill/models/sale_order.py
+++ iet_improve_po_create_bill/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import models, api
+from odoo import models, api,fields
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
@@ -14,3 +14,40 @@ class SaleOrderLine(models.Model):
                 line.analytic_distribution = {
                     str(order.analytic_account_id.id): 100
                 }
+
+    name = fields.Text(
+        string="Description",
+        compute='_compute_name',
+        store=True, readonly=False, required=True, precompute=True)
+
+    @api.depends('product_id', 'linked_line_id', 'linked_line_ids')
+    def _compute_name(self):
+        for line in self:
+            if not line.product_id and not line.is_downpayment:
+                continue
+
+            lang = line.order_id._get_lang()
+            if lang != self.env.lang:
+                line = line.with_context(lang=lang)
+
+            if line.product_id:
+                line.name = ''
+                print(f'Product {line.product_id.name}')
+                print(f'Product {line.name}')
+                continue
+
+            if line.is_downpayment:
+                line.name = line._get_downpayment_description()
+
+    @api.onchange("product_id")
+    def _onchange_product_id_clear_name(self):
+        for line in self:
+            if line.product_id and not line.is_downpayment:
+                line.name = False
+                print(f'line,anme from on change==>{line.name}')
+
+    def create(self, vals_list):
+        for vals in vals_list:
+            print(f"Manual name entered: {vals_list}")
+            print(f"Manual name entered: {vals.get('name')}")
+        return super().create(vals_list)
