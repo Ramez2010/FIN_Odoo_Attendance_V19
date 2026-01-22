a860d85f0628299a1f094503e16ad3194fe5562f
Ramez2010 <154975867+Ramez2010@users.noreply.github.com>
2026-01-22 08:20:49 +0200

    Allow post init hook to accept env

--- odoo_attendance_app/hooks.py
+++ odoo_attendance_app/hooks.py
@@ -8,8 +8,14 @@ def _safe_unlink(env, xmlid):
         record.unlink()
 
 
-def post_init_cleanup_transfer_actions(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_cleanup_transfer_actions(env_or_cr, registry=None):
+    """
+    Accept both Odoo 18 signature (cr, registry) and Odoo 19 signature (env).
+    """
+    if registry is None and hasattr(env_or_cr, 'registry'):
+        env = env_or_cr
+    else:
+        env = api.Environment(env_or_cr, SUPERUSER_ID, {})
     _safe_unlink(env, 'odoo_attendance_app.action_attendance_timesheet_transfer_wizard')
     _safe_unlink(env, 'odoo_attendance_app.action_attendance_timesheet_transfer_from_list')
     _safe_unlink(env, 'odoo_attendance_app.menu_odoo_attendance_timesheet_transfer')
