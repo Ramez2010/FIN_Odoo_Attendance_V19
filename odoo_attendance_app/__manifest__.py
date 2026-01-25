b8781d56688078f7275a2e8111e2655a09b465b1
Ramez2010 <154975867+Ramez2010@users.noreply.github.com>
2026-01-25 22:14:58 +0200

    Move setup guide to bottom and expand layout

--- odoo_attendance_app/__manifest__.py
+++ odoo_attendance_app/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'FIN Attendance',
-    'version': '19.0.1.0.10',
+    'version': '19.0.1.0.11',
     'category': 'Human Resources',
     'summary': 'Mobile attendance tracking with GPS, analytics, and time-off management',
     'description': """
